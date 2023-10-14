4c1f4b285a18ff6de6fa4a86e5f29009732454a7
Sax <127007056+Aussachaa@users.noreply.github.com>
2023-10-14 14:48:03 +0700

    add2

--- dashborad.py
+++ dashborad.py
@@ -17,13 +17,15 @@ st.markdown(
 
 fl = st.file_uploader(":file_folder: Upload a file",
                       type=(["csv", "txt", "xlsx", "xls"]))
+
+path_file = r"https://raw.githubusercontent.com/Aussachaa/FirstStreamlit/main/Sample%20-%20Superstore.csv"
 if fl is not None:
     filename = fl.name
     st.write(filename)
     df = pd.read_csv(filename, encoding="ISO-8859-1")
 else:
-    os.chdir(r"C:\Users\aussa\OneDrive\เดสก์ท็อป\Python\Streamlit\FirstStreamlit")
-    df = pd.read_csv("Sample - Superstore.csv", encoding="ISO-8859-1")
+    # os.chdir(r"C:\Users\aussa\OneDrive\เดสก์ท็อป\Python\Streamlit\FirstStreamlit")
+    df = pd.read_csv(path_file, encoding="ISO-8859-1")
 
 
 col1, col2 = st.columns((2))
