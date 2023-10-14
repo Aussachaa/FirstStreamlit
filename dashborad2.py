25adab285e2caf916d293bf7c29ac6adbba96e59
Sax <127007056+Aussachaa@users.noreply.github.com>
2023-10-14 18:41:42 +0700

    asd

--- dashborad2.py
+++ dashborad2.py
@@ -33,7 +33,7 @@ col1, col2 = st.columns((2))
 # df["Order Date"] = pd.to_datetime(df["Order Date"])
 
 # Getting the min and max date
-startDate = pd.to_datetime(df["Order Date"])
+startDate = pd.to_datetime(df["Order Date"]).dt.date
 # endDate = pd.to_datetime(df["Order Date"]).max()
 
 st.write(startDate)
